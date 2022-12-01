6ad1aceb15c7ea10f5d4a1f4fb86971a8c3de323
PamelaMichelle <pmm.guananga@yavirac.edu.ec>
2022-11-30 23:27:18 -0500

    creacion de productos y catalogo

--- proyecto1/src/app/app.module.ts
+++ proyecto1/src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+import { StaticModule } from './static/static.module';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { PagesComponent } from './pages/pages.component';
+import { StaicComponent } from './static/staic.component';
 import { UserComponent } from './user/user/user.component';
 
-
-import { NotFoundComponent } from './not-found/not-found.component';
-
-import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
-
-
-import { StaicComponent } from './static/staic.component';
-import { AuthComponent } from './auth/auth.component';
-
-import { StaticModule } from './static/static.module';
-import { PagesModule } from './pages/pages.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
